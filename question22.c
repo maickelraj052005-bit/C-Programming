306f6acd6529f6b45506ccfd7ae5675908d6a81f
maickelraj S <maickelraj052005@gmail.com>
2026-01-25 17:39:52 +0530

    Add files via upload

---
+++ question22.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+   int num ;
+   scanf("%d\n",&num);
+   if (num%2==0)
+   {
+    printf("This number is even%d",num);
+    else;
+        printf("This number is odd %d",num);
+    }
+   
+   
+    return 0;
+}
