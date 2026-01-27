763d59e2327e80098a5f4e65b615874b38ca9df2
maickelraj S <maickelraj052005@gmail.com>
2026-01-27 23:09:21 +0530

    Add files via upload

--- question15.c
+++ question15.c
@@ -1,9 +1,60 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-    int a= 10;
-    int b= 5;
-    printf("%d\n",a-b);
-    return 0;
+    int mode;
+    char cat;
+    int fee;
 
+    printf("Enter Mode (1-Online, 2-Offline): ");
+    scanf("%d", &mode);
+
+    printf("Enter Category (R/S): ");
+    scanf(" %c", &cat);  
+    switch(mode)
+    {
+        case 1:   
+
+            switch(cat)
+            {
+                case 'R':
+                    fee = 5000;
+                    break;
+
+                case 'S':
+                    fee = 3000;
+                    break;
+
+                default:
+                    printf("Invalid Category");
+                    return 0;
+            }
+            break;
+
+        case 2:   
+
+            switch(cat)
+            {
+                case 'R':
+                    fee = 9000;
+                    break;
+
+                case 'S':
+                    fee = 7000;
+                    break;
+
+                default:
+                    printf("Invalid Category");
+                    return 0;
+            }
+            break;
+
+        default:
+            printf("Invalid Mode");
+            return 0;
+    }
+
+    printf("₹%d", fee);
+
+    return 0;
 }
--- question16.c
+++ question16.c
@@ -1,9 +1,40 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-    int a= 4;
-    int b= 5;
-    printf("%d\n",a*b);
-    return 0;
+    int cls, weight;
+    int charge = 0;
 
+    printf("Enter Flight Class (1-Economy, 2-Business): ");
+    scanf("%d", &cls);
+
+    printf("Enter Extra Weight (kg): ");
+    scanf("%d", &weight);
+
+    switch(cls)
+    {
+        case 1: 
+            charge = weight * 300;
+            printf("Extra Baggage Charge ₹%d", charge);
+            break;
+
+        case 2:
+
+            if(weight <= 3)
+            {
+                printf("Free");
+            }
+            else
+            {
+                charge = (weight - 3) * 300;
+                printf("Extra Baggage Charge ₹%d", charge);
+            }
+
+            break;
+
+        default:
+            printf("Invalid Class");
+    }
+
+    return 0;
 }
