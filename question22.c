f47435a28922d36caa8a0d81bae3171d5bf8365e
maickelraj S <maickelraj052005@gmail.com>
2026-01-29 09:17:11 +0530

    Add files via upload

--- question22.c
+++ question22.c
@@ -1,14 +1,19 @@
-#include<stdio.h>
-int main(){
-   int num ;
-   scanf("%d\n",&num);
-   if (num%2==0)
-   {
-    printf("This number is even%d",num);
-    else;
-        printf("This number is odd %d",num);
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+    scanf("%d", &n);
+
+    for (int i = 2; i <= n; i++) {
+        int isPrime = 1;
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+        if (isPrime) count++;
     }
-   
-   
+    printf("%d", count);
     return 0;
 }
