1f5d1e455411a769d443f18f68e3dcfe53c0fe6a
maickelraj S <maickelraj052005@gmail.com>
2026-02-18 22:42:06 +0530

    Add files via upload

---
+++ Function/function7.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int prime(int a)
+{
+    if (a<=1)
+    {
+        return 0;
+    }
+    for (int i = 2; i*i < a; i++)
+    {
+        if (a%i==0)
+        {
+            return 0;
+        }
+        
+    }
+    return 1;
+}
+void main()
+{
+    int a;
+    scanf("%d",&a);
+    if (prime(a))
+    {
+        printf("Prime");
+    }
+    else
+    {
+        printf("Not Prime");
+
+    }
+    
+}
