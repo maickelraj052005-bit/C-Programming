e43ff3c38fdad7f5e84685522c54c3ea1a8446b5
maickelraj S <maickelraj052005@gmail.com>
2026-02-18 22:43:26 +0530

    Add files via upload

---
+++ Function/function11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void table(int n)
+{
+    for(int i=1;i<=10;i++)
+        printf("%d x %d = %d\n", i, n, i*n);
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+   table(n) ;
+}
