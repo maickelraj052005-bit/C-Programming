29f1e43edb256ce6902a36570fa7891cc3f45747
maickelraj S <maickelraj052005@gmail.com>
2026-02-18 22:41:26 +0530

    Add files via upload

---
+++ Function/function5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int digits (int a)
+{
+    
+    int sum=0;
+    
+    while (a!=0)
+    {
+        sum=sum+(a%10);
+        a=a/10;
+    }
+    return sum;
+    
+}
+void main ()
+{
+    int a;
+    scanf("%d",&a);
+    printf("%d", digits(a));
+}
