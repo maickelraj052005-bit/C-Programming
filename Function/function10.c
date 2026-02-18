f90061dc860b676cd33e52888040a12a3008c14c
maickelraj S <maickelraj052005@gmail.com>
2026-02-18 22:43:06 +0530

    Add files via upload

---
+++ Function/function10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int random(int n)
+{
+    int sum = 0;
+
+    for(int i=1; i<=n; i++)
+        sum += i;
+
+    return sum;
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    printf("%d", random(n));
+}
