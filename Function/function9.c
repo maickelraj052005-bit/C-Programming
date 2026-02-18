caaf58bdd980fd6a7fca44e84079bc1f97788b9f
maickelraj S <maickelraj052005@gmail.com>
2026-02-18 22:42:46 +0530

    Add files via upload

---
+++ Function/function9.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int reverse(int n)
+{
+    int r=0;
+    while(n!=0)
+    {
+        r = r*10 + n%10;
+        n = n/10;
+    }
+    return r;
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    if(n == reverse(n))
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+}
