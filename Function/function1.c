1cec121137891ee79f7b4633385f996b1510db72
maickelraj S <maickelraj052005@gmail.com>
2026-02-18 22:38:48 +0530

    Add files via upload

---
+++ Function/function1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int interest()
+{
+    int p,r,t;
+    scanf("%d %d %d",&p,&r,&t);
+
+    int c = (p*r*t)/100;
+    
+
+    return c;
+
+}
+
+void main()
+{
+    printf("%d",interest());
+}
