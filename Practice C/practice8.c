6932ede7f742d3d2e2dbbf6d9510f781f72df5e6
maickelraj S <maickelraj052005@gmail.com>
2026-02-25 22:10:24 +0530

    Add files via upload

---
+++ Practice C/practice8.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+
+    printf("With Temp ");
+    int a=10;
+    int b=20;
+    int temp=0;
+
+    printf("Before swapping \n");
+    printf("%d %d \n",a,b);
+    printf("After swapping \n");
+
+    temp=a;
+    a=b;
+    b=temp;
+
+    printf("%d %d",a,b);
+
+    return 0;
+}
