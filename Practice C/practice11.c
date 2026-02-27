11474657236e9c1691f95fa3266375f0c3d3e5a4
maickelraj S <maickelraj052005@gmail.com>
2026-02-27 22:05:01 +0530

    Add files via upload

---
+++ Practice C/practice11.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int num,rem,rev=0,original;
+    scanf("%d",&num);
+    
+    original = num;
+
+    while (num!=0)
+    {
+        rem=num%10;
+        rev=rev*10+rem;
+        num=num/10;
+    }
+
+    if (original==rev)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("false");
+    }
+    
+    return 0;
+    
+}
